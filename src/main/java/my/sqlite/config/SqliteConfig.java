1b08203c02fc5c5f29b397b1e72edb55290ba12d
petterobam <1460300366@qq.com>
2018-04-27 22:34:22 +0800

    修改丰富配置文件和配置类，扩展支持默认配置

--- src/main/java/my/sqlite/config/SqliteConfig.java
+++ src/main/java/my/sqlite/config/SqliteConfig.java
@@ -1,37 +1,21 @@
 package my.sqlite.config;
 
+import org.sqlite.SQLiteConfig;
+
 import java.io.IOException;
 import java.util.Properties;
 
 /**
  * sqlite的一些静态配置
  */
-public class SqliteConfig {
-    /**
-     * 程序数据库
-     */
-    public static final String DB_PATH = "database/sqlite.db";
-    public static final String DB_PATH1 = "database/sqlite1.db";
-    public static final String DB_PATH2 = "database/sqlite2.db";
-    public static final String DB_PATH3 = "database/sqlite3.db";
-    /**
-     * 测试数据库
-     */
-    public static final String TEST_DB_PATH = "database/test.db";
-    /**
-     * 程序数据库动态生成规则
-     */
-    public static final int DB_TYPE_DEFAULT = 0;//不分库
-    public static final int DB_TYPE_BY_MINUTE = 1;//按分钟自动分库
-    public static final int DB_TYPE_BY_HOUR = 2;//按小时自动分库
-    public static final int DB_TYPE_BY_DAY = 3;//按天自动分库
-    public static final int DB_TYPE_BY_MOUTH = 4;//按月自动分库
-    public static final int DB_TYPE_BY_YEAR = 5;//按年自动分库
+public class SqliteConfig{
 
     /**
      * sqlite配置文件的配置信息
      */
     private static Properties properties = new Properties();
+
+    private static SQLiteConfig config = null;
 
     /**
      * 启动程序的时候读取properties配置文件信息，并永久缓存
@@ -41,8 +25,38 @@ public class SqliteConfig {
             //使用 properties 配置文件，默认在 config/sqlite.properties 目录下面，若该项目被引用，启动项目只需要在相同目录下相同配置文件覆盖即可生效
             properties.load(SqliteConfig.class.getClassLoader().getResourceAsStream("config/sqlite.properties"));
             //properties.loadFromXML(SqliteConfig.class.getClassLoader().getResourceAsStream("config/sqlite.xml"));
+            //创建Sqlite API 配置对象
+            config = new SQLiteConfig(properties);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * 单例模式获取配置对象
+     * @return
+     */
+    public SQLiteConfig getConfig(){
+        if(null == config){
+            config = new SQLiteConfig(properties);
+        }
+        return config;
+    }
+
+    /**
+     * 动态加载新配置
+     * @param classPath
+     */
+    public void loadConfig(String classPath){
+        try {
+            //使用 properties 配置文件，默认在 config/sqlite.properties 目录下面，若该项目被引用，启动项目只需要在相同目录下相同配置文件覆盖即可生效
+            properties.load(SqliteConfig.class.getClassLoader().getResourceAsStream(classPath));
+            //properties.loadFromXML(SqliteConfig.class.getClassLoader().getResourceAsStream("config/sqlite.xml"));
+            //创建Sqlite API 配置对象
+            config = new SQLiteConfig(properties);
+        } catch (IOException e) {
+            e.printStackTrace();
+            properties = new Properties();
         }
     }
 
